43d457b4c9c089c952e398863b29fc75297bf96f
Wei Chen Chen <WiZ@ibm.com>
2025-03-03 12:16:16 +0800

    Initial commit

---
+++ src/test/java/application_operation/ParkFlow/ParkFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package application_operation.ParkFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
